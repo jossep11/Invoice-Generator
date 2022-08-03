e9914959fb82adf221830f9cafcee4bb39b3360b
JossepP <jossepparedes@gmail.com>
2022-08-02 22:03:46 -0400

    clientid mayor a 3 facturas

--- index.js
+++ index.js
@@ -5,42 +5,34 @@ let billing = JSON.parse(rawdata);
 // console.log(Object.values(billing.data));
 // console.log(billing.data[Object.keys(billing.data)[100]].clientid);
 let objectManage = Object.keys(billing.data);
-// let data = billing.data;
-// console.log(data[objectManage[0]]);
-// const res = data.reduce((acc, curr, index) => {
-//   console.log(curr);
-// });
+
 let newArray = [];
 
 for (let index = 0; index < objectManage.length; index++) {
-  //   console.log(
-  //     billing.data[objectManage[index]].clientid,
-  //     billing.data[objectManage[index]].amount_unpaid
-  //   );
   newArray.push({
     clientid: billing.data[objectManage[index]].clientid,
     amount_unpaid: billing.data[objectManage[index]].amount_unpaid,
   });
 }
+
 let counts = {};
 newArray.forEach(function (x) {
-  // clientid: (x.clientid || 0) + 1,
-  // amount_unpaid: x.amount_unpaid,
   counts[x.clientid] = (counts[x.clientid] || 0) + 1;
 });
 
-console.log(counts);
+const resultado = {};
+newArray.forEach(
+  (el, index) => (resultado[el.clientid] = resultado[el.clientid] + 1 || 1)
+);
 
-// Object.values(counts).forEach((x, index) => {
-//   if (x >= 3) {
-//     // console.log(index);
-//     console.log(newArray[index]);
-//     console.log(x, ":", index);
-//   }
-// });
-
-// console.log(counts);
-// console.log(newArray);
+Object.keys(resultado).forEach((element, index) => {
+  if (Object.values(resultado)[index] >= 3) {
+    console.log(element);
+    // i should add this to a new array so then i can compare it with the other array and see if they are more than 3 billing so i can sum them
+  } else {
+    // console.log("no hay");
+  }
+});
 
 // const res = newArray.reduce((acc, curr, index) => {
 //   if (curr.clientid in acc) {
