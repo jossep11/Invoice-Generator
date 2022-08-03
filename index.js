00d96900089944ac7c442b047a6067e94b9c9269
JossepP <jossepparedes@gmail.com>
2022-08-03 15:44:03 -0400

    sum and filter of users with more than 3 invoices

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require("fs");
-let rawdata = fs.readFileSync("response7meses.json");
-let billing = JSON.parse(rawdata);
+let rawData = fs.readFileSync("response7meses.json");
+let billing = JSON.parse(rawData);
 
-// console.log(Object.values(billing.data));
-// console.log(billing.data[Object.keys(billing.data)[100]].clientid);
 let objectManage = Object.keys(billing.data);
 
 let newArray = [];
@@ -15,24 +13,29 @@ for (let index = 0; index < objectManage.length; index++) {
   });
 }
 
-let counts = {};
-newArray.forEach(function (x) {
-  counts[x.clientid] = (counts[x.clientid] || 0) + 1;
-});
-
-const resultado = {};
+const timesRepeatedClientID = {};
 newArray.forEach(
-  (el, index) => (resultado[el.clientid] = resultado[el.clientid] + 1 || 1)
+  (el, index) =>
+    (timesRepeatedClientID[el.clientid] =
+      timesRepeatedClientID[el.clientid] + 1 || 1)
 );
-
-Object.keys(resultado).forEach((element, index) => {
-  if (Object.values(resultado)[index] >= 3) {
-    console.log(element);
-    // i should add this to a new array so then i can compare it with the other array and see if they are more than 3 billing so i can sum them
-  } else {
-    // console.log("no hay");
+let ClientidR3T = [];
+Object.keys(timesRepeatedClientID).forEach((element, index) => {
+  if (Object.values(timesRepeatedClientID)[index] >= 3) {
+    ClientidR3T.push(element);
   }
 });
+let total = [];
+ClientidR3T.forEach((element) => {
+  let Clients = newArray.filter((el) => el.clientid === element);
+  total.push(Clients);
+  console.log(
+    element,
+    Clients.reduce((a, b) => parseFloat(a) + parseFloat(b.amount_unpaid), 0)
+  );
+});
+
+console.log(total.length);
 
 // const res = newArray.reduce((acc, curr, index) => {
 //   if (curr.clientid in acc) {
